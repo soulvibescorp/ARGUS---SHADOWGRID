6a2347d560780a4d87b77bf8305546924b146f26
soulvibescorp <113645673+soulvibescorp@users.noreply.github.com>
2025-04-24 03:29:54 -0400

    Update BehavioralModule.js

--- js/BehavioralModule.js
+++ js/BehavioralModule.js
@@ -13,3 +13,22 @@ function BehavioralModule({ data }) {
 }
 
 export default BehavioralModule;
+
+import React from 'react';
+
+const BehavioralModule = ({ data }) => {
+    return (
+        <div className="module-card">
+            <h2>Behavioral Insights</h2>
+            <ul>
+                {data.map((user, index) => (
+                    <li key={index}>
+                        <strong>{user.name}</strong> - Interests: {user.interests.join(', ')} - Sentiment: {user.sentiment}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default BehavioralModule;
