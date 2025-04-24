046ab6305c2c959b16ebbfb00777d29f0c194be6
soulvibescorp <113645673+soulvibescorp@users.noreply.github.com>
2025-04-24 03:43:41 -0400

    Update BehavioralModule.js

--- js/BehavioralModule.js
+++ js/BehavioralModule.js
@@ -1,21 +1,5 @@
 import React from 'react';
 
-function BehavioralModule({ data }) {
-    return (
-        <div className="module-card">
-            <h3>Behavioral Profiling</h3>
-            <div className="data">
-                <p>Total Profiles: {data.length}</p>
-                {/* Add charts or heatmaps to visualize the behavioral data */}
-            </div>
-        </div>
-    );
-}
-
-export default BehavioralModule;
-
-import React from 'react';
-
 const BehavioralModule = ({ data }) => {
     return (
         <div className="module-card">
